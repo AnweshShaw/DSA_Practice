055979be16f84073415703363f108eab18298265
Anwesh Shaw <83204279+AnweshShaw@users.noreply.github.com>
2023-02-01 17:50:59 +0530

    Add files via upload

---
+++ Linked_List/ll.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val) // Initialising the head pointer of the linked list
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+// Insertion at beginning of Linked List
+void insertAthead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+// Insertion at end of Linked List
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    node *temp = head;
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+bool search(node *head, int key)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+void deleteAtHead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+}
+
+void deletion(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    if (head->next == NULL)
+    {
+        deleteAtHead(head);
+        return;
+    }
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+}
+
+node *reverse(node *&head)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+    while (currptr != NULL)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+    return prevptr;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 10);
+    insertAtTail(head, 20);
+    insertAtTail(head, 30);
+    display(head);
+    insertAthead(head, 40);
+    display(head);
+    cout << search(head, 10) << endl;
+    deletion(head, 30);
+    display(head);
+    deleteAtHead(head);
+    display(head);
+    node *newhead = reverse(head);
+    display(newhead);
+}
