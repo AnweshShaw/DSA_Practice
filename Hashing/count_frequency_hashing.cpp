ce7929bc6e79a3463f3136b4778c1e877b7a3275
Anwesh Shaw <83204279+AnweshShaw@users.noreply.github.com>
2023-04-14 17:32:57 +0530

    Update count_frequency_hashing.cpp

--- Hashing/count_frequency_hashing.cpp
+++ Hashing/count_frequency_hashing.cpp
@@ -1,6 +1,37 @@
 #include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <map>
+#include <unordered_map>
+
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define ff first
+#define ss second
+#define setBits(x) builtin_popcount(x)
 using namespace std;
 
 signed main()
 {
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    map<int, int> freq;
+    rep(i, 0, n)
+    {
+        freq[a[i]]++;
+    }
+
+    map<int, int>::iterator it;
+    for (it = freq.begin(); it != freq.end(); it++)
+    {
+        cout << it->ff << " " << it->ss << " " << endl;
+    }
 }
